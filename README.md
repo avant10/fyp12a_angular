eca5c68e30c3c5a09dcf17cb0ba1181d51081bfc
avant10 <aavant10.z@gmail.com>
2020-05-06 13:37:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Login and Upload FrontEnd Angular 9
+upload file components had been added via ng g c addblog 
+## 
+
+Manually clone the repo and then run `npm install` to install modules
+
+##
+npm start
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './home.component.html'
+
+})
+export class HomeComponent {
+  url = 'https://nationalinterest.org/sites/default/files/main_images/M9-pistolet%20%281%29.jpg'
+  selectedFile: File = null;
+  constructor(private http: HttpClient){}
+  onFileSelected(event) {
+    this.selectedFile = event.target.files[0];
+    const reader = new FileReader()
+    reader.readAsDataURL(event.target.files[0])
+    reader.onload = (event : any) => {
+      this.url = event.target.result;
+
+    }
+
+  }
+  onDownload(){
+
+
+
+  }
+
+
+
+}
